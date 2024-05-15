7aed88c4e319cda14983662dff1ce4d3baa467b0
Shubham Krishna shinde <118288116+shubhamSS9@users.noreply.github.com>
2024-05-15 06:45:06 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,182 @@
+# name = ["shubham",67, "krishna",98, "shinde"]
+# print(name)
+
+# animal = ['zebra', 17171.8, 18]
+# # print(animal)
+# ages = [26, 27, 29, 32]
+# print(animal[1], ages[3])
+# print((animal[:1], ages[:-1])*5)
+
+# animal = ["lion", "tiger", "cat", "dog", "racon"]
+# i = 0
+# while i < len(animal):
+#     print(animal[i])
+#     i = i + 1
+    
+# for i, a in enumerate(animal):
+#     print(i,a)
+
+# animal = ["lion", "tiger", "cat", "dog", "racon"]
+# ages = [26, 27, 29, 32, 23, 27, 24]
+
+# animal[2] = "horse"
+# print(animal)
+# ages[5] = 26
+# print(ages)
+# ages[2:] = ["shubham"]
+# print(ages)
+# ages[2:5] = []
+# print(ages)
+
+# lst = [23, 45, 34, 56, 21, 32]
+# lst = lst + [627,0, 983, 200]
+# print(lst)
+
+# s = [10, 20, 30]
+# t = [100, 200, 300]
+# z = s + t
+# print(z)
+
+# l = list("shubham")
+# print(l)
+
+# n = list("9730909275")
+# print(n)
+
+# lst1 = [30, 49, 25, 787, 76, 100, 21, 97]
+# lst2 = lst1
+# # print(lst2)
+
+# lst1[0] = 10000
+# print(lst2)
+# print(lst1)
+
+
+# lst1 = [30, 49, 25, 787, 76, 100, 21, 97]
+# animal = ["lion", "tiger", "cat", "dog", "racon"]
+
+# print(25 in lst1)
+
+# num1 = 10
+# num2 = 10
+
+# s1 = "shubham"
+# s2 = "shubham"
+
+# print(num1 is num2)
+# print(s1 is s2)
+
+# print(num1 == num2)
+# print(s1 == s2)
+
+# lst = []
+# if lst:
+#     print("Empty list")
+
+# lst = [21, 353,765, 83897]
+# print(bool(lst))
+
+
+
+# Built-in functions in list
+
+# animal = ["lion", "tiger", "cat", "dog", "raccon", "zebra", "apes"]
+# ages = [26, 27, 29, 32, 23, 27, 24]
+# lst=[]
+
+
+# print(len(animal))
+# print(max(animal))
+# print(min(animal))
+# print(sum(ages))
+# print(any(lst))
+# print(all(lst))
+# print(sorted(ages))
+# print(sorted(animal))
+# print(sorted(lst))
+# print(reversed(animal))
+# print(reversed(ages))
+
+
+
+
+# animal = ["lion", "tiger", "cat", "dog", "raccon", "zebra", "apes"]
+
+# print(animal.append("shubham"))
+# print(animal.remove("cat"))
+# print(animal.pop(3))
+
+
+
+# ages = [26, 27, 29, 32, 23, 27, 24]
+
+# ages.append(10)
+# print(ages)
+
+# ages.remove(24)
+# print(ages)
+
+# ages.pop(2)
+# print(ages)
+
+# ages.insert(1, 76176)
+# print(ages)
+
+# ages.count(29)
+# print(ages.count(27))
+
+# print(ages.index(24))
+# ages.insert(0, 777)
+# print(ages)
+
+# lst = [10, 5, 0, 58, 97]
+# # lst.reverse()
+# # print(lst)
+
+# lst.sort()
+# print(lst)
+
+# lst.sort(reverse=1)
+# print(lst)
+
+# # print(lst.sort())
+
+# a = [10, 3, 76, 90]
+# b = [5, 8, 0, 8, 54]
+# c = [a, b]
+# d = [56, a, 78, b, 899, c]
+# print(d)
+
+# s = "shubham"
+# l = [*s]
+# print(l)
+
+# x = [1, 3, 2, 4, 5, 67]
+
+# y = [45, 8, 90, 876, *x, 897]
+# print(y)
+
+# lst = ["Anil", "Amol", "Aditya", "Avi", "Alka"]
+
+# lst.insert(2, "Anuj")
+# print(lst)
+
+# lst.append("Zulu")
+# print(lst)
+
+# lst.remove("Avi")
+# print(lst)
+
+# # a = lst.index("Anil")
+# lst[0] = "AnilKumar"
+# print(lst)
+
+# lst.sort()
+# print(lst)
+
+# # print(lst.sort())
+# lst.reverse()
+# print(lst)
+
+
+
